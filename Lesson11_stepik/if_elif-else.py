409e2500ec7bb8b65fee254ccd463eefc0a2cb09
Николай <Kolya_popov_86@tut.by>
2022-11-16 22:34:10 +0300

    Lesson_11_if_elif_else

---
+++ Lesson11_stepik/if_elif-else.py
@@ -0,0 +1,83 @@
+# # if - уловный оператор
+# x = -4
+# if x < 0:
+#     x = -x
+# print(x)
+#
+# a = float(input('a: '))
+# b = float(input('b: '))
+# if a < b:
+#     a, b = b, a
+# print(a, b)
+#
+# x = int(input())
+#
+# if x >= -4 and x <= 10:
+#     print('x  в диапазоне [-4; 10]')
+# #
+# marks = [3, 3, 4, 4, 4]
+#
+# if 2 in marks:
+#     print('Студент будет отчислен')
+# else:
+#     print('Студент успешно сдал сессию')
+#
+# x = int(input())
+#
+# if x < 0:
+#     print('х - отрицательное число')
+# else:
+#     print('x - не отрицательное число')
+
+# x = int(input())
+#
+# if x % 2 == 0:
+#     if 0 <= x <= 9:
+#         print('x - это цифра')
+#     else:
+#         print('x - число')
+# else:
+#     print('x - нечётное число')
+
+# a = int(input('a:'))
+# b = int(input('b:'))
+# c = int(input('c:'))
+#
+# if a > b:
+#     if a > c:
+#         print('a -> max')
+#     else:
+#         print('c -> max')
+# else:
+#     if b > c:
+#         print('b -> max')
+#     else:
+#         print('c -> max')
+
+# item = int(input())
+#
+# if item == 1:
+#     print('Курс по Python')
+# elif item == 2:
+#     print('Курс по С++')
+# elif item == 3:
+#     print('Курс по Java')
+# elif item == 4:
+#     print('Курс по JavaScript')
+# else:
+#     print('Ничего не выбрано')
+
+# c = int(input())
+#
+# if c < 0:
+#     print('число должно быть положительным')
+# elif 0 <= c <= 9:
+#     print('c - это число')
+# elif 10 <= c <= 99:
+#     print('c - двухзначное число')
+# elif 100 <= c <= 999:
+#     print('c - тречзначное число')
+# else:
+#     print('c - что то иное')
+
+
